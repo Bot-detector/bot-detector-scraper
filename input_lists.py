5e138a02a46fa889c17fda3dbf352a33eaa3d00f
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-09-15 12:37:15 -0400

    Forgot a comma,,,,,,,,,,,,,,,

--- input_lists.py
+++ input_lists.py
@@ -64,7 +64,7 @@ hiscores_minigames = ["league",
                       "kril_tsutsaroth",
                       "mimic",
                       "nightmare",
-                      "	phosanis_nightmare"
+                      "phosanis_nightmare",
                       "obor",
                       "sarachnis",
                       "scorpia",
