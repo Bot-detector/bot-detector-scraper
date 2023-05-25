552a572a8eb9baac8ed37caf6737c430c30d065d
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-05-26 00:05:03 +0200

    less moving data around

--- src/helpers/worker.py
+++ src/helpers/worker.py
@@ -8,6 +8,7 @@ import time
 from helpers.api import botDetectorApi
 import config
 from typing import TYPE_CHECKING
+from aiohttp import ClientSession
 
 logger = logging.getLogger(__name__)
 
@@ -17,66 +18,54 @@ if TYPE_CHECKING:
 
 class NewWorker:
     def __init__(self, proxy, manager) -> None:
-        self.scraper = Scraper(proxy)
+        self.scraper: Scraper = Scraper(proxy)
         self.manager: Manager = manager
-        self.name = str(uuid.uuid4())
-        self.api = botDetectorApi(
+        self.name: str = str(uuid.uuid4())
+        self.api: botDetectorApi = botDetectorApi(
             config.ENDPOINT, config.QUERY_SIZE, config.TOKEN, config.MAX_BYTES
         )
-        self.active = True
+        self.active: bool = True
 
     async def run(self, timeout: int):
         async with aiohttp.ClientSession(timeout=timeout) as session:
             while self.active:
-                # check if it has to get new players
-                # check if it has to post players
-                # scrape players
                 if await self.manager.get_players_task():
-                    # asyncio.create_task(self._get_data())
                     await self._get_data()
                 elif await self.manager.get_post_task():
                     data = await self.manager.get_post_data()
                     await self._post_data(data)
                     self.manager.post_lock = False
-                    # asyncio.create_task(self._post_data(data))
                 else:
-                    player: dict = await self.manager.get_player()
+                    player = await self.manager.get_player()
                     if player is None:
                         logger.info(f"worker={self.name} is idle.")
                         await asyncio.sleep(60)
                     else:
-                        scraped_data = await self._scrape_data(session, player)
-                        if scraped_data is not None:
-                            await self.manager.add_highscores(scraped_data)
+                        await self._scrape_data(session, player)
                 await asyncio.sleep(1)
 
-    async def _scrape_data(self, session: aiohttp.ClientSession, player: dict):
+    async def _scrape_data(self, session: ClientSession, player: dict):
         hiscore = await self.scraper.lookup_hiscores(player, session)
 
         if hiscore == "ClientHttpProxyError":
             logger.warning(f"ClientHttpProxyError killing worker name={self.name}")
             self.active = False
             return
-        
-        # data validation
+
         if hiscore is None:
             logger.warning(f"Hiscore is empty for {player.get('name')}")
             return
 
-        # player is not on the hiscores
         if "error" in hiscore:
-            # update additional metadata
             player["possible_ban"] = 1
             player["confirmed_player"] = 0
             player = await self.scraper.lookup_runemetrics(player, session)
         else:
-            # update additional metadata
             player["possible_ban"] = 0
             player["confirmed_ban"] = 0
             player["label_jagex"] = 0
             player["updated_at"] = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 
-        # data validation
         if player is None:
             logger.warning(f"Player is None, Player_id: {hiscore.get('Player_id')}")
             return
@@ -85,29 +74,21 @@ class NewWorker:
             "player": player,
             "hiscores": None if "error" in hiscore else hiscore,
         }
-        return output
+        await self.manager.add_highscores(output)
 
     async def _post_data(self, data):
         try:
             logger.info(f"worker={self.name} post scraped players")
-            # get a copy of the highscores data to be posted to bot detector API
-            # post the data to bot detector API
             await self.api.post_scraped_players(data)
         except Exception as e:
-            logger.error(f"{str(e)}")
-            # wait for 60 seconds and try again
+            logger.error(str(e))
             await asyncio.sleep(60)
-        return
 
     async def _get_data(self):
         try:
             logger.info(f"worker={self.name} get players to scrape")
-            # get players from bot detector API
             players = await self.api.get_players_to_scrape()
             await self.manager.add_players(players)
         except Exception as e:
-            logger.error(f"{str(e)}")
-            # wait for 60 seconds and try again
+            logger.error(str(e))
             await asyncio.sleep(60)
-            return
-        return
