d14c1907a83618228a361e556f8fc9ca052aed82
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-06-26 20:59:46 +0200

    topic config

--- src_kafka/cleanup.py
+++ src_kafka/cleanup.py
@@ -7,14 +7,27 @@ def main():
     print(topics)
     if not topics == []:
         admin_client.delete_topics(topics)
-    res = admin_client.create_topics([
-        NewTopic(name="player", num_partitions=12, replication_factor=1, topic_configs={}),
-        NewTopic(name="scraper", num_partitions=2, replication_factor=1, topic_configs={}),
-    ])
+    res = admin_client.create_topics(
+        [
+            NewTopic(
+                name="player",
+                num_partitions=12,
+                replication_factor=1,
+                topic_configs={"retention.ms": 86_400_000, "cleanup.policy": "delete"},
+            ),
+            NewTopic(
+                name="scraper",
+                num_partitions=2,
+                replication_factor=1,
+                topic_configs={"retention.ms": 86_400_000, "cleanup.policy": "delete"},
+            ),
+        ]
+    )
 
     print(res)
     topics = admin_client.list_topics()
     print(topics)
+
 
 if __name__ == "__main__":
     main()
