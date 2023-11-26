4fa461fe6d8e5a74a8d33da89f67d1b6c121f5fe
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-11-27 00:23:32 +0100

    Update cleanup.py

--- src_kafka/cleanup.py
+++ src_kafka/cleanup.py
@@ -15,7 +15,7 @@ def main():
         [
             NewTopic(
                 name="player",
-                num_partitions=20,
+                num_partitions=4,
                 replication_factor=1,
                 topic_configs={
                     # "session.timeout.ms":60000
